662750272dd88de08fe9280e927261e515dcbd7c
aroulis Lamprogeorgos <vissarionlamp@gmail.com>
2024-01-27 09:42:00 +0200

    Updated files

--- Whatisyourname/Program.vb
+++ Whatisyourname/Program.vb
@@ -1,17 +1,16 @@
 Imports System
 
 Module Program
-    Public num1 As Integer
-    Public num2 As Integer
-    Public answer As Integer
     Sub Main(args As String())
-        Console.Write("Type a number and press Enter")
-        num1 = Console.ReadLine()
-        Console.Write("Type another number to add to it and press Enter")
-        num2 = Console.ReadLine()
-        answer = num1 + num2
-        Console.WriteLine("The answer is " & answer)
-        Console.Write("Press any key to continue...")
-        Console.ReadKey(True)
+
+        Console.WriteLine("Hi.Please enter password:")
+        Dim code = Console.ReadLine()
+        Console.WriteLine("Please enter password again:")
+        If Console.ReadLine = code Then
+            Console.WriteLine("Succes!")
+
+
+        End If
+
     End Sub
 End Module
