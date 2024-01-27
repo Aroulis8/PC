92f5e01a71a5f0b7ee168e08dd5edd34b4c7cc93
aroulis Lamprogeorgos <vissarionlamp@gmail.com>
2024-01-27 16:39:47 +0200

    Updated Program.vb

--- Whatisyourname/Program.vb
+++ Whatisyourname/Program.vb
@@ -1,15 +1,22 @@
 Imports System
+Imports System.Threading
 
 Module Program
     Sub Main(args As String())
 
-        Console.WriteLine("Hi.Please enter password:")
+        Console.WriteLine("Hi.Please enter name:")
+        Dim name = Console.ReadLine()
+        Console.WriteLine("Please Wait...")
+        Thread.Sleep(5000)
+        Console.WriteLine($"Welcome back {name}!")
+        Thread.Sleep(3000)
+        Console.WriteLine("Please enter password:")
         Dim code = Console.ReadLine()
-        Console.WriteLine("Please enter password again:")
-        If Console.ReadLine = code Then
-            Console.WriteLine("Succes!")
+        Thread.Sleep(3000)
+        Console.Clear()
+        Console.WriteLine("Login Successful!")
 
-        End If
+
 
     End Sub
 End Module
